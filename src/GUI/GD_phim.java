6983b5215a337b3e5e5976cc7425a46fe249e785
Grooger04 <luannguyen01022004@gmail.com>
2024-11-10 23:13:21 +0700

    thêm xóa sửa thoải mái

--- src/GUI/GD_phim.java
+++ src/GUI/GD_phim.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import DuLieuTam.ThongTinVe_Tam;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -46,6 +47,7 @@ public class GD_phim extends JPanel {
 	private JLabel Label7_2_1,Label7_2_2,Label7_2_3,Label7_1;
 	private JLabel Label8_2_1,Label8_2_2,Label8_2_3,Label8_1;
 	private JPanel panel1,panel2,panel3,panel4,panel5,panel6,panel7,panel8;
+	private ThongTinVe_Tam tam = new ThongTinVe_Tam();
 
 	public GD_phim(Menu_view menu) {
 		
@@ -53,15 +55,50 @@ public class GD_phim extends JPanel {
 		setBackground(new Color(225, 214, 196));
 		setLayout(null);
 		
+		// panel 1
+		panel1 = new JPanel();
+		panel1.setBounds(120, 20, 160, 270);
+		add(panel1);
+		panel1.setLayout(null);
+		//panel 2
+		panel2 = new JPanel();
+		panel2.setLayout(null);
+		panel2.setBounds(400, 20, 160, 270);
+		add(panel2);
+		//panel 3
+		panel3 = new JPanel();
+		panel3.setLayout(null);
+		panel3.setBounds(680, 20, 160, 270);
+		add(panel3);
+		//panel 4
+		panel4 = new JPanel();
+		panel4.setLayout(null);
+		panel4.setBounds(960, 20, 160, 270);
+		add(panel4);
+		//panel 5
+		panel5 = new JPanel();
+		panel5.setLayout(null);
+		panel5.setBounds(120, 320, 160, 270);
+		add(panel5);
+		//panel 6
+		panel6 = new JPanel();
+		panel6.setLayout(null);
+		panel6.setBounds(400, 320, 160, 270);
+		add(panel6);
+		//panel 7
+		panel7 = new JPanel();
+		panel7.setLayout(null);
+		panel7.setBounds(680, 320, 160, 270);
+		add(panel7);
+		//panel 8
+		panel8 = new JPanel();
+		panel8.setLayout(null);
+		panel8.setBounds(960, 320, 160, 270);
+		add(panel8);
+		
 		for(int i=0; i< danhsachphim.size(); i++) {
 			phim[i] = danhsachphim.get(i);
-			if(i==0) {
-				// panel 1
-				panel1 = new JPanel();
-				panel1.setBounds(120, 20, 160, 270);
-				add(panel1);
-				panel1.setLayout(null);
-				
+			if(i==0 && phim[i]!=null) {
 				Label1_1 = new JLabel();
 				Label1_1.setBackground(new Color(0, 64, 64));
 				Label1_1.setBounds(0, 0, 160, 190);
@@ -73,25 +110,31 @@ public class GD_phim extends JPanel {
 				Label1_2_1.setBounds(0, 190, 160, 20);
 				panel1.add(Label1_2_1);
 				
-				JLabel Label1_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				JLabel Label1_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong() + " phút");
 				Label1_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label1_2_2.setBounds(0, 210, 160, 20);
 				panel1.add(Label1_2_2);
 				
-				Label1_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label1_2_3 = new JLabel("Giá vé:" + phim[i].getGiave() + " VND");
 				Label1_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label1_2_3.setBounds(0, 230, 160, 20);
 				panel1.add(Label1_2_3);
 				
-				
+				btnMua1 = new JButton("Mua");
+				btnMua1.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua1.setBounds(45, 250, 70, 20);
+				btnMua1.setBackground(new Color(138, 63, 63));
+				panel1.add(btnMua1);
+				btnMua1.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==1) {
-				//panel 2
-				panel2 = new JPanel();
-				panel2.setLayout(null);
-				panel2.setBounds(400, 20, 160, 270);
-				add(panel2);
-				
+			else if(i==1 && phim[i]!=null) {
 				Label2_1 = new JLabel();
 				Label2_1.setBackground(new Color(0, 64, 64));
 				Label2_1.setBounds(0, 0, 160, 190);
@@ -103,26 +146,31 @@ public class GD_phim extends JPanel {
 				Label2_2_1.setBounds(0, 190, 160, 20);
 				panel2.add(Label2_2_1);
 				
-				JLabel Label2_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				JLabel Label2_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label2_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label2_2_2.setBounds(0, 210, 160, 20);
 				panel2.add(Label2_2_2);
 				
-				Label2_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label2_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label2_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label2_2_3.setBounds(0, 230, 160, 20);
 				panel2.add(Label2_2_3);
 				
-			
-				
+				btnMua2 = new JButton("Mua");
+				btnMua2.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua2.setBackground(new Color(138, 63, 63));
+				btnMua2.setBounds(45, 250, 70, 20);
+				panel2.add(btnMua2);
+				btnMua2.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==2) {
-				//panel 3
-				panel3 = new JPanel();
-				panel3.setLayout(null);
-				panel3.setBounds(680, 20, 160, 270);
-				add(panel3);
-				
+			else if(i==2 && phim[i]!=null) {
 				Label3_1 = new JLabel();
 				Label3_1.setBackground(new Color(0, 64, 64));
 				Label3_1.setBounds(0, 0, 160, 190);
@@ -135,56 +183,70 @@ public class GD_phim extends JPanel {
 				Label3_2_1.setBounds(0, 190, 160, 20);
 				panel3.add(Label3_2_1);
 				
-				Label3_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label3_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label3_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label3_2_2.setBounds(0, 210, 160, 20);
 				panel3.add(Label3_2_2);
 				
-				Label3_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label3_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label3_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label3_2_3.setBounds(0, 230, 160, 20);
 				panel3.add(Label3_2_3);
 				
-				
-				
+				btnMua3 = new JButton("Mua");
+				btnMua3.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua3.setBackground(new Color(138, 63, 63));
+				btnMua3.setBounds(45, 250, 70, 20);
+				panel3.add(btnMua3);
+				btnMua3.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						String maTam = phim[2].getMaPhim();
+						tam.setMaPhim_Tam(maTam);
+						System.out.println(tam.getMaPhim_Tam());
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==3) {
-				//panel 4
-				panel4 = new JPanel();
-				panel4.setLayout(null);
-				panel4.setBounds(960, 20, 160, 270);
-				add(panel4);
-				
+			else if(i==3 && phim[i]!=null) {
 				Label4_1 = new JLabel();
 				Label4_1.setBackground(new Color(0, 64, 64));
 				Label4_1.setBounds(0, 0, 160, 190);
 				panel4.add(Label4_1);
 				Label4_1.setIcon(new ImageIcon(phim[i].getDuongdan().trim()));
 				
-				Label4_2_1 = new JLabel("Tên Phim" + phim[i].getTenPhim().trim());
+				Label4_2_1 = new JLabel("Tên Phim:" + phim[i].getTenPhim().trim());
 				Label4_2_1.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label4_2_1.setBounds(0, 190, 160, 20);
 				panel4.add(Label4_2_1);
 				
-				Label4_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label4_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label4_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label4_2_2.setBounds(0, 210, 160, 20);
 				panel4.add(Label4_2_2);
 				
-				Label4_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label4_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label4_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label4_2_3.setBounds(0, 230, 160, 20);
 				panel4.add(Label4_2_3);
 				
-			
+				btnMua4 = new JButton("Mua");
+				btnMua4.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua4.setBackground(new Color(138, 63, 63));
+				btnMua4.setBounds(45, 250, 70, 20);
+				panel4.add(btnMua4);
+				btnMua4.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==4) {
-				//panel 5
-				panel5 = new JPanel();
-				panel5.setLayout(null);
-				panel5.setBounds(120, 320, 160, 270);
-				add(panel5);
-				
+			else if(i==4 && phim[i]!=null) {
 				Label5_1 = new JLabel();
 				Label5_1.setBackground(new Color(0, 64, 64));
 				Label5_1.setBounds(0, 0, 160, 190);
@@ -196,26 +258,32 @@ public class GD_phim extends JPanel {
 				Label5_2_1.setBounds(0, 190, 160, 20);
 				panel5.add(Label5_2_1);
 				
-				Label5_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label5_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label5_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label5_2_2.setBounds(0, 210, 160, 20);
 				panel5.add(Label5_2_2);
 				
-				Label5_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label5_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label5_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label5_2_3.setBounds(0, 230, 160, 20);
 				panel5.add(Label5_2_3);
+			
+				btnMua5 = new JButton("Mua");
+				btnMua5.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua5.setBackground(new Color(138, 63, 63));
+				btnMua5.setBounds(45, 250, 70, 20);
+				panel5.add(btnMua5);
 				
-				
+				btnMua5.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 				
 			}
-			else if(i==5) {
-				//panel 6
-				panel6 = new JPanel();
-				panel6.setLayout(null);
-				panel6.setBounds(400, 320, 160, 270);
-				add(panel6);
-				
+			else if(i==5 && phim[i]!=null) {
 				Label6_1 = new JLabel();
 				Label6_1.setBackground(new Color(0, 64, 64));
 				Label6_1.setBounds(0, 0, 160, 190);
@@ -227,25 +295,32 @@ public class GD_phim extends JPanel {
 				Label6_2_1.setBounds(0, 190, 160, 20);
 				panel6.add(Label6_2_1);
 				
-				Label6_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label6_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label6_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label6_2_2.setBounds(0, 210, 160, 20);
 				panel6.add(Label6_2_2);
 				
-				Label6_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label6_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label6_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label6_2_3.setBounds(0, 230, 160, 20);
 				panel6.add(Label6_2_3);
 				
+				btnMua6 = new JButton("Mua");
+				btnMua6.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua6.setBackground(new Color(138, 63, 63));
+				btnMua6.setBounds(45, 250, 70, 20);
+				panel6.add(btnMua6);
 				
+				btnMua6.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==6) {
-				//panel 7
-				panel7 = new JPanel();
-				panel7.setLayout(null);
-				panel7.setBounds(680, 320, 160, 270);
-				add(panel7);
-				
+			else if(i==6 && phim[i]!=null) {
 				Label7_1 = new JLabel();
 				Label7_1.setBackground(new Color(0, 64, 64));
 				Label7_1.setBounds(0, 0, 160, 190);
@@ -257,25 +332,32 @@ public class GD_phim extends JPanel {
 				Label7_2_1.setBounds(0, 190, 160, 20);
 				panel7.add(Label7_2_1);
 				
-				Label7_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label7_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label7_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label7_2_2.setBounds(0, 210, 160, 20);
 				panel7.add(Label7_2_2);
 				
-				Label7_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label7_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label7_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label7_2_3.setBounds(0, 230, 160, 20);
 				panel7.add(Label7_2_3);
 				
+				btnMua7 = new JButton("Mua");
+				btnMua7.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua7.setBackground(new Color(138, 63, 63));
+				btnMua7.setBounds(45, 250, 70, 20);
+				panel7.add(btnMua7);
 				
+				btnMua7.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						menu.chaneToGD_ve();
+					}
+				});
 			}
-			else if(i==7) {
-				//panel 8
-				panel8 = new JPanel();
-				panel8.setLayout(null);
-				panel8.setBounds(960, 320, 160, 270);
-				add(panel8);
-				
+			else if(i==7 && phim[i]!=null) {
 				Label8_1 = new JLabel();
 				Label8_1.setBackground(new Color(0, 64, 64));
 				Label8_1.setBounds(0, 0, 160, 190);
@@ -287,70 +369,23 @@ public class GD_phim extends JPanel {
 				Label8_2_1.setBounds(0, 190, 160, 20);
 				panel8.add(Label8_2_1);
 				
-				Label8_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong());
+				Label8_2_2 = new JLabel("Thời Lượng:" + phim[i].getThoiLuong()+" phút");
 				Label8_2_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label8_2_2.setBounds(0, 210, 160, 20);
 				panel8.add(Label8_2_2);
 				
-				Label8_2_3 = new JLabel("Giá vé:" + phim[i].getGiave());
+				Label8_2_3 = new JLabel("Giá vé:" + phim[i].getGiave()+" VND");
 				Label8_2_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
 				Label8_2_3.setBounds(0, 230, 160, 20);
 				panel8.add(Label8_2_3);
 				
-			}
-		}
-			btnMua1 = new JButton("Mua");
-			btnMua1.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua1.setBounds(45, 250, 70, 20);
-			btnMua1.setBackground(new Color(138, 63, 63));
-			panel1.add(btnMua1);
-			
-			btnMua2 = new JButton("Mua");
-			btnMua2.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua2.setBackground(new Color(138, 63, 63));
-			btnMua2.setBounds(45, 250, 70, 20);
-			panel2.add(btnMua2);
-			
-			btnMua3 = new JButton("Mua");
-			btnMua3.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua3.setBackground(new Color(138, 63, 63));
-			btnMua3.setBounds(45, 250, 70, 20);
-			panel3.add(btnMua3);
-			
-			btnMua4 = new JButton("Mua");
-			btnMua4.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua4.setBackground(new Color(138, 63, 63));
-			btnMua4.setBounds(45, 250, 70, 20);
-			panel4.add(btnMua4);
-			
-			btnMua5 = new JButton("Mua");
-			btnMua5.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua5.setBackground(new Color(138, 63, 63));
-			btnMua5.setBounds(45, 250, 70, 20);
-			panel5.add(btnMua5);
-			
-			btnMua6 = new JButton("Mua");
-			btnMua6.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua6.setBackground(new Color(138, 63, 63));
-			btnMua6.setBounds(45, 250, 70, 20);
-			panel6.add(btnMua6);
-			
-			btnMua7 = new JButton("Mua");
-			btnMua7.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua7.setBackground(new Color(138, 63, 63));
-			btnMua7.setBounds(45, 250, 70, 20);
-			panel7.add(btnMua7);
-			
-			btnMua8 = new JButton("Mua");
-			btnMua8.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
-			btnMua8.setBackground(new Color(138, 63, 63));
-			btnMua8.setBounds(45, 250, 70, 20);
-			panel8.add(btnMua8);
-			
-			//khởi tạo chuyển trang cho các nút button
-			JButton[] buttons = {btnMua1,btnMua2,btnMua3,btnMua4,btnMua5,btnMua6,btnMua7,btnMua8};
-			for(int k = 0; k< buttons.length; k++) {
-				buttons[k].addActionListener(new ActionListener() {
+				btnMua8 = new JButton("Mua");
+				btnMua8.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+				btnMua8.setBackground(new Color(138, 63, 63));
+				btnMua8.setBounds(45, 250, 70, 20);
+				panel8.add(btnMua8);
+				
+				btnMua8.addActionListener(new ActionListener() {
 					
 					@Override
 					public void actionPerformed(ActionEvent e) {
@@ -358,8 +393,10 @@ public class GD_phim extends JPanel {
 						menu.chaneToGD_ve();
 					}
 				});
+				
 			}
-
+			
+		}	
 	}
 
 }
